0fa86f5cd130fd7677a9e40e05f1883890e21845
atournayre <aurelien.tournayre@gmail.com>
2021-12-31 15:01:17 +0100

    Préparation de SynchronisationAgenda.

---
+++ Traits/SynchronisationAgenda.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Atournayre\Component\Domain\Traits;
+
+// TODO Embedded
+// TODO Embedded
+// TODO Embedded
+// TODO Embedded
+trait SynchronisationAgenda
+{
+    protected $identifiantAgenda;
+    protected $identifiantEvenement;
+    protected $identifiantService;
+
+}
