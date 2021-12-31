9b512c13f25553419d1b9a211c0654b7d2b9f27c
atournayre <aurelien.tournayre@gmail.com>
2021-12-31 19:53:18 +0100

    Add Name constraint.

---
+++ Constraint/Name/Name.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Atournayre\Component\Domain\Constraints\Name;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+class Name extends Constraint
+{
+}
